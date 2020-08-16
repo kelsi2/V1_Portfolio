9f6887507bbdb273ccf50064ef86aca34d2423fb
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-08-16 16:47:08 -0700

    added files

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const packageJSON = require('../package.json');
 const path = require('path');
 const sh = require('shelljs');
+require('dotenv').config()
 
 module.exports = function renderScripts() {
     const sourcePath = path.resolve(path.dirname(__filename), '../src/js/scripts.js');
     const destPath = path.resolve(path.dirname(__filename), '../dist/js/scripts.js');
-    
+
     const copyright = `/*!
     * Start Bootstrap - ${packageJSON.title} v${packageJSON.version} (${packageJSON.homepage})
     * Copyright 2013-${new Date().getFullYear()} ${packageJSON.author}
@@ -16,10 +17,10 @@ module.exports = function renderScripts() {
     `
     const scriptsJS = fs.readFileSync(sourcePath);
     const destPathDirname = path.dirname(destPath);
-    
+
     if (!sh.test('-e', destPathDirname)) {
         sh.mkdir('-p', destPathDirname);
     }
-    
+
     fs.writeFileSync(destPath, copyright + scriptsJS);
 };
